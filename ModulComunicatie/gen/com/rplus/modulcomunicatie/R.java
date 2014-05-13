c45f440ac062ecd6f302c179b8177eca3373a4b9
RTerciu <radu_rt25@yahoo.com>
2014-05-13 18:21:56 +0300

    HTTP Request catre ruta de GetData
    Hardcodat pe ip-ul 192.168.1.132.
    
    Async Task care face get-ul pe ruta corespunzatoare, cu api key-ul
    introdus si cu userToken-ul generat.

--- ModulComunicatie/gen/com/rplus/modulcomunicatie/R.java
+++ ModulComunicatie/gen/com/rplus/modulcomunicatie/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1046,6 +1046,7 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int textView1=0x7f05003d;
         public static final int textView2=0x7f050041;
+        public static final int textView3=0x7f050042;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1150,6 +1151,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int conectat=0x7f0a0013;
         public static final int crypt_button=0x7f0a0010;
         public static final int crypt_text=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
--- ModulComunicatie/src/com/rplus/modulcomunicatie/MainActivity.java
+++ ModulComunicatie/src/com/rplus/modulcomunicatie/MainActivity.java
@@ -1,21 +1,38 @@
 package com.rplus.modulcomunicatie;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+	TextView RaspunsJson;
+	TextView conectat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +59,24 @@ public class MainActivity extends ActionBarActivity {
             	String userToken=md5(initialToken+format);
             	tv.setText(initialToken+format+" "+userToken);
             	
-            	
+            	RaspunsJson = (TextView) findViewById(R.id.textView2);
+            	conectat=(TextView)findViewById(R.id.textView3);
+                //tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
+         
+                // check if you are connected or not
+                if(isConnected()){
+                	conectat.setBackgroundColor(0xFF00CC00);
+                    conectat.setText("Esti Conectat la Internet!");
+                }
+                else{
+                   Toast.makeText(getApplicationContext(), "Nu esti conectat la Internet",Toast.LENGTH_SHORT).show();
+                   conectat.setBackgroundColor(0xFFFF0000);
+                   conectat.setText("Nu esti conectat la Internet!");
+                }
+         
+                // call AsynTask to perform network operation on separate thread
+                new HttpAsyncTask().execute("http://192.168.1.132/Rplus_Server/public/getData/"+et1.getText().toString()+"/"+userToken+"/"+apiKey);
+                
             	
             	
             	
@@ -52,6 +86,76 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    
+    
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+ 
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+ 
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+ 
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+ 
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+ 
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+ 
+        return result;
+    }
+ 
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+ 
+        inputStream.close();
+        return result;
+ 
+    }
+ 
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) 
+                return true;
+            else
+                return false;   
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+ 
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            RaspunsJson.setText(result);
+       }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     public static final String md5(final String s) {
         final String MD5 = "MD5";
         try {
